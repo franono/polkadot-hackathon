b3684518741a965bdb6faff17565397df204ae4d
Francesco <cremdev@gmail.com>
2020-11-02 01:00:12 +0100

    update address

--- ink-erc20/frontend/src/ChickenContract.js
+++ ink-erc20/frontend/src/ChickenContract.js
@@ -2,7 +2,7 @@ import metadata from './ChickenMetadata.json';
 import {Abi, ContractPromise} from '@polkadot/api-contract';
 
 export const defaultGasLimit = 300000n * 1000000n;
-const ChickenContractAddress = '5FiaR7iSTV8pnjpr7NQmwHSyN5jmJg22yFtqhsWQwrw6bX9M';
+const ChickenContractAddress = '5Ef8uv187oYvkQx2X9eeSBXJB9bFzCHs6fiWhFvuxeQW77YF';
 
 export default function ChickenContract(api) {
     const abi = new Abi(metadata);
